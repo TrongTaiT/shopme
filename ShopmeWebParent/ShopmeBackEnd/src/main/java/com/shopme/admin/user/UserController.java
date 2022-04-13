04fb13d40b0d2a4f18b4a662df183b71e2a434cc
TrongTai <nguyentrongtai.ait@gmail.com>
2022-04-14 00:29:36 +0700

    Section 6: Code Update User Function

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -14,15 +15,15 @@ import com.shopme.common.entity.User;
 
 @Controller
 public class UserController {
-	
+
 	@Autowired
 	private UserService service;
-	
+
 	@GetMapping("/users")
 	public String listAll(Model model) {
 		List<User> listUsers = service.listAll();
 		model.addAttribute("listUsers", listUsers);
-		
+
 		return "users";
 	}
 
@@ -35,6 +36,7 @@ public class UserController {
 
 		model.addAttribute("user", user);
 		model.addAttribute("listRoles", listRoles);
+		model.addAttribute("pageTitle", "Create New User");
 
 		return "user_form";
 	}
@@ -45,6 +47,25 @@ public class UserController {
 
 		redirectAttributes.addFlashAttribute("message", "User saved successfully");
 		return "redirect:/users";
+	}
+
+	@GetMapping("/users/edit/{id}")
+	public String editUser(@PathVariable(name = "id") Integer id, //
+			Model model, //
+			RedirectAttributes redirectAttributes) {
+		try {
+			User user = service.get(id);
+			List<Role> listRoles = service.listRoles();
+			
+			model.addAttribute("user", user);
+			model.addAttribute("pageTitle", "Edit User (ID: " + id + ")");
+			model.addAttribute("listRoles", listRoles);
+			
+			return "user_form";
+		} catch (UserNotFoundException e) {
+			redirectAttributes.addFlashAttribute("message", e.getMessage());
+			return "redirect:/users";
+		}
 	}
 
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -1,6 +1,7 @@
 package com.shopme.admin.user;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,7 +18,7 @@ public class UserService {
 
 	@Autowired
 	private RoleRepository roleRepo;
-	
+
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 
@@ -30,19 +31,64 @@ public class UserService {
 	}
 
 	public void save(User user) {
-		encodePassword(user);
+		boolean isUpdatingUser = (user.getId() != null);
+		
+		if (isUpdatingUser) {
+			User existingUser = userRepo.findById(user.getId()).get();
+			
+			if (user.getPassword().isEmpty()) {
+				user.setPassword(existingUser.getPassword());
+			} else {
+				encodePassword(user);
+			}
+		} else {
+			encodePassword(user);
+		}
+		
 		userRepo.save(user);
 	}
-	
+
 	private void encodePassword(User user) {
 		String encodePassword = passwordEncoder.encode(user.getPassword());
 		user.setPassword(encodePassword);
 	}
-	
-	public boolean isEmailUnique(String email) {
+
+	public boolean isEmailUnique(Integer id, String email) {
 		User userByEmail = userRepo.getUserByEmail(email);
-		
-		return userByEmail == null;
+
+//		if (userByEmail == null)
+//			return true;
+//
+//		boolean isCreatingNew = (id == null);
+//
+//		if (isCreatingNew) {
+//			if (userByEmail != null)
+//				return false;
+//		} else {
+//			if (userByEmail.getId() != id)
+//				return false;
+//		}
+		if (userByEmail != null) {
+			// creating
+			if (id == null) {
+				return false;
+			}
+			// updating
+			// 2 different users but the same email
+			if (userByEmail.getId() != id) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public User get(Integer id) throws UserNotFoundException {
+		try {
+			return userRepo.findById(id).get();
+		} catch (NoSuchElementException e) {
+			throw new UserNotFoundException("Could not find any user with ID " + id);
+		}
 	}
 
 }
