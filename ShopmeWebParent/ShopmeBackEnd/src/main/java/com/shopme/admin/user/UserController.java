b19e3684221fe0911597f511ccd9c515e1a8a8e1
TrongTai <nguyentrongtai.ait@gmail.com>
2022-04-16 03:09:12 +0700

    Section 6: Code User Photos Upload Function

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -1,15 +1,20 @@
 package com.shopme.admin.user;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopme.admin.FileUploadUtil;
 import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
@@ -42,8 +47,24 @@ public class UserController {
 	}
 
 	@PostMapping("/users/save")
-	public String saveUser(User user, RedirectAttributes redirectAttributes) {
-		service.save(user);
+	public String saveUser(User user, RedirectAttributes redirectAttributes,
+			@RequestParam("image") MultipartFile multipartFile) throws IOException //
+	{
+		if (!multipartFile.isEmpty()) {
+			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+			user.setPhotos(fileName);
+			User savedUser = service.save(user);
+			
+			String uploadDir = "user-photos/" + savedUser.getId();
+
+			FileUploadUtil.cleanDir(uploadDir);
+			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+		} else {
+			if (user.getPhotos().isEmpty()) {
+				user.setPhotos(null);
+			}
+			service.save(user);
+		}
 
 		redirectAttributes.addFlashAttribute("message", "User saved successfully");
 		return "redirect:/users";
@@ -75,8 +96,7 @@ public class UserController {
 	{
 		try {
 			service.delete(id);
-			redirectAttributes.addFlashAttribute("message",
-					"The user ID " + id + " has been deleted successfully");
+			redirectAttributes.addFlashAttribute("message", "The user ID " + id + " has been deleted successfully");
 		} catch (UserNotFoundException e) {
 			redirectAttributes.addFlashAttribute("message", e.getMessage());
 		}
@@ -84,9 +104,7 @@ public class UserController {
 	}
 
 	@GetMapping("/users/{id}/enabled/{status}")
-	public String updateUserEnabledStatus(
-			@PathVariable("id") Integer id,
-			@PathVariable("status") Boolean enabled,
+	public String updateUserEnabledStatus(@PathVariable("id") Integer id, @PathVariable("status") Boolean enabled,
 			RedirectAttributes redirectAttributes) //
 	{
 		service.updateUserEnabledStatus(id, enabled);
