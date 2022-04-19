937840b28e54f62c4d249416aab90ca3a69cfef6
TrongTai <nguyentrongtai.ait@gmail.com>
2022-04-20 01:31:40 +0700

    Section 6: 60. Code User Authorization Basics

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -42,6 +42,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+			.antMatchers("/users/**").hasAuthority("Admin")
+			.antMatchers("/categories/**").hasAuthority("Admin")
+			
 			.anyRequest() //.permitAll();
 			.authenticated()
 			.and()
