0ba1375edd35cfbd5b24f897038fd57187bdf4a6
TrongTai <nguyentrongtai.ait@gmail.com>
2022-06-18 19:20:13 +0700

    90. Exercise: Code Brand Form

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Brand.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Brand.java
@@ -12,6 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "brands")
@@ -83,6 +84,14 @@ public class Brand {
 
 	public void setCategories(Set<Category> categories) {
 		this.categories = categories;
+	}
+
+	@Transient
+	public String getLogoPath() {
+		if (this.id == null)
+			return "/images/image-thumbnail.png";
+
+		return "/brand-logos/" + this.id + "/" + this.logo;
 	}
 
 }
