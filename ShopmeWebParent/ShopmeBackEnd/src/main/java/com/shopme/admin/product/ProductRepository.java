569320eb7724ea8c0233e93804bae92593e83cd5
TrongTai <nguyentrongtai.ait@gmail.com>
2022-06-19 17:29:10 +0700

    108. Exercise: Update Product Enabled Status

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.shopme.admin.product;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,10 @@ import com.shopme.common.entity.Product;
 @Repository
 public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
 
-	public Product findByName(String name);	
-	
+	public Product findByName(String name);
+
+	@Query("UPDATE Product p SET p.enabled = ?2 WHERE p.id = ?1")
+	@Modifying
+	public void updateEnabledStatus(Integer id, Boolean enabled);
+
 }
