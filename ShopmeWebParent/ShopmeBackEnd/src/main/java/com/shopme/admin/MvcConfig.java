40753f698b55017474fe41250fedcb77c42c99b6
TrongTai <nguyentrongtai.ait@gmail.com>
2022-06-19 11:30:53 +0700

    97. Code Refactor #5

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfig.java
@@ -13,31 +13,23 @@ public class MvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		// user photos
-		String dirName = "user-photos";
-		Path userPhotosDir = Paths.get(dirName);
-
-		String userPhotosPath = userPhotosDir.toFile().getAbsolutePath();
-
-		registry.addResourceHandler("/" + dirName + "/**") //
-				.addResourceLocations("file:/" + userPhotosPath + "/");
+		exposeDirectory("user-photos", registry);
 
 		// category images
-		String categoryImagesDirName = "../category-images";
-		Path categoryImagesDir = Paths.get(categoryImagesDirName);
-
-		String categoryImagesPath = categoryImagesDir.toFile().getAbsolutePath();
-
-		registry.addResourceHandler("/category-images/**") //
-				.addResourceLocations("file:/" + categoryImagesPath + "/");
+		exposeDirectory("../category-images", registry);
 
 		// brand logos
-		String brandLogosDirName = "../brand-logos";
-		Path brandLogosDir = Paths.get(brandLogosDirName);
+		exposeDirectory("../brand-logos", registry);
+	}
 
-		String brandLogosPath = brandLogosDir.toFile().getAbsolutePath();
+	private void exposeDirectory(String pathPattern, ResourceHandlerRegistry registry) {
+		Path path = Paths.get(pathPattern);
+		String absolutePath = path.toFile().getAbsolutePath();
 
-		registry.addResourceHandler("/brand-logos/**") //
-				.addResourceLocations("file:/" + brandLogosPath + "/");
+		String logicalPath = pathPattern.replace("../", "") + "/**";
+
+		registry.addResourceHandler(logicalPath) //
+				.addResourceLocations("file:/" + absolutePath + "/");
 	}
 
 }
