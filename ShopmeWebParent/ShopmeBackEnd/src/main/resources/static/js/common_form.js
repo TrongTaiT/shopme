ea09d08482eddb165d0d7fb8d1c557fc4b50aadd
TrongTai <nguyentrongtai.ait@gmail.com>
2022-06-19 21:18:04 +0700

    112. Code Add Extra Images on Product Form

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form.js
@@ -4,14 +4,7 @@ $(document).ready(function () {
     });
 
     $("#fileImage").change(function () {
-        fileSize = this.files[0].size;
-
-        if (fileSize > MAX_FILE_SIZE) {
-            this.setCustomValidity("You must choose an image less than " + MAX_FILE_SIZE + " bytes!");
-            this.reportValidity();
-        }
-        else {
-            this.setCustomValidity("");
+        if (checkFileSize(this)) {
             showImageThumbnail(this);
         }
     });
@@ -27,6 +20,19 @@ function showImageThumbnail(fileInput) {
     reader.readAsDataURL(file);
 }
 
+function checkFileSize(fileInput) {
+    fileSize = fileInput.files[0].size;
+
+    if (fileSize > MAX_FILE_SIZE * 1024) {
+        fileInput.setCustomValidity("You must choose an image less than " + MAX_FILE_SIZE + "KB!");
+        fileInput.reportValidity();
+        return false;
+    }
+    else {
+        fileInput.setCustomValidity("");
+        return true;
+    }
+}
 
 function showModalDialog(title, message) {
     $("#modalTitle").html(title);
