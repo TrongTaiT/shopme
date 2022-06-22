d7718f4d15dac58b753e01dc939d4db533acee7d
TrongTai <nguyentrongtai.ait@gmail.com>
2022-06-23 03:56:21 +0700

    141. Exercise: Code Repository Layer for Country and State

---
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/setting/CountryRepositoryTests.java
@@ -0,0 +1,82 @@
+package com.shopme.admin.setting;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.common.entity.Country;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class CountryRepositoryTests {
+
+	@Autowired
+	private CountryRepository repo;
+
+	@Test
+	public void testCreateCountry1() {
+		Country country = new Country("Vietnam", "vn");
+
+		Country savedCountry = repo.save(country);
+
+		assertThat(savedCountry.getId()).isGreaterThan(0);
+	}
+
+	@Test
+	public void testCreateCountry2() {
+		Country country = new Country("United State", "us");
+
+		Country savedCountry = repo.save(country);
+
+		assertThat(savedCountry.getId()).isGreaterThan(0);
+	}
+
+	@Test
+	public void testListCountries() {
+		Iterable<Country> result = repo.findAllByOrderByNameAsc();
+
+		result.forEach(System.out::println);
+
+		assertThat(result).size().isGreaterThan(0);
+	}
+
+	@Test
+	public void testGetCountry() {
+		Integer id = 1;
+		Country country = repo.findById(id).get();
+
+		assertThat(country).isNotNull();
+	}
+
+	@Test
+	public void testUpdateCountry() {
+		Integer id = 1;
+		String newName = "Việt Nam";
+		Country country = repo.findById(id).get();
+
+		country.setName(newName);
+
+		Country updatedCountry = repo.save(country);
+
+		assertThat(updatedCountry.getName()).isEqualTo(newName);
+	}
+
+	@Test
+	public void testDeleteCountry() {
+		Integer id = 2;
+		repo.deleteById(id);
+
+		Optional<Country> result = repo.findById(id);
+
+		assertThat(result).isEmpty();
+	}
+
+}
---
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/setting/StateRepositoryTests.java
@@ -0,0 +1,103 @@
+package com.shopme.admin.setting;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.common.entity.Country;
+import com.shopme.common.entity.State;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class StateRepositoryTests {
+
+	@Autowired
+	private StateRepository repo;
+
+	@Autowired
+	private EntityManager entityManager;
+
+	@Test
+	public void testCreateState1() {
+		Country vn = entityManager.find(Country.class, 3);
+		State state = new State("Lâm Đồng", vn);
+
+		State savedState = repo.save(state);
+
+		assertThat(savedState.getId()).isGreaterThan(0);
+	}
+
+	@Test
+	public void testCreateState2() {
+		Country us = entityManager.find(Country.class, 4);
+		State state = new State("California", us);
+
+		State savedState = repo.save(state);
+
+		assertThat(savedState.getId()).isGreaterThan(0);
+	}
+
+	@Test
+	public void testCreateState3() {
+		Country us = entityManager.find(Country.class, 4);
+		State state = new State("Virginia", us);
+
+		State savedState = repo.save(state);
+
+		assertThat(savedState.getId()).isGreaterThan(0);
+	}
+
+	@Test
+	public void testListStatesByCountry() {
+		Country us = entityManager.find(Country.class, 4);
+
+		List<State> listStates = repo.findByCountryOrderByNameAsc(us);
+
+		listStates.forEach(System.out::println);
+
+		assertThat(listStates.size()).isGreaterThan(0);
+	}
+
+	@Test
+	public void testGetState() {
+		Integer id = 2;
+		Optional<State> result = repo.findById(id);
+
+		assertThat(result).isNotEmpty();
+	}
+
+	@Test
+	public void testUpdateState() {
+		Integer id = 2;
+		String newName = "Alabama";
+
+		State state = repo.findById(id).get();
+		state.setName(newName);
+
+		State updatedState = repo.save(state);
+
+		assertThat(updatedState.getName()).isEqualTo(newName);
+	}
+
+	@Test
+	public void testDeleteState() {
+		Integer id = 1;
+		repo.deleteById(id);
+
+		Optional<State> result = repo.findById(id);
+
+		assertThat(result).isEmpty();
+	}
+
+}
