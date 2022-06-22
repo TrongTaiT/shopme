c344fa2e99397e1b895aa9a9fd3fcf7b80519dd2
TrongTai <nguyentrongtai.ait@gmail.com>
2022-06-22 19:13:02 +0700

    133. Code Product Images Slideshow

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
@@ -16,6 +16,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -75,6 +76,7 @@ public class Product {
 	private Brand brand;
 
 	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+	@OrderBy("id asc")
 	private Set<ProductImage> images = new HashSet<>();
 
 	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
