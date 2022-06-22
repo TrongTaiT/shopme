1132cde6f8b195bd1acc8359751d36de4494df79
TrongTai <nguyentrongtai.ait@gmail.com>
2022-06-23 02:47:36 +0700

    140. Apply General Settings for Shopping Application

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/test/java/com/shopme/setting/SettingRepositoryTests.java
@@ -0,0 +1,27 @@
+package com.shopme.setting;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.shopme.common.entity.Setting;
+import com.shopme.common.entity.SettingCategory;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class SettingRepositoryTests {
+
+	@Autowired
+	private SettingRepository repo;
+
+	@Test
+	public void testFindByTwoCategories() {
+		List<Setting> settings = repo.findByTwoCategories(SettingCategory.GENERAL, SettingCategory.CURRENCY);
+		settings.forEach(System.out::println);
+	}
+
+}
